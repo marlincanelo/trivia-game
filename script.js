865c946d9c82e61317f42f86cdf76dfe98129641
mailin y daileni <darlenycanelo@macbookair.bmcc.cuny.edu>
2026-06-10 13:31:49 -0400

    phase 3: question data model and loadQuestion render function

--- script.js
+++ script.js
@@ -1,3 +1,93 @@
+const questions = [
+  {
+    text: "What is Adobe After Effects mainly used for?",
+    answers: [
+      "Video effects and motion graphics",
+      "Writing code",
+      "Image Compression",
+      "Web Hosting"
+    ],
+    correct: 0  // index of the correct answer in the answers array
+  },
+  {
+    text: "Which tool is commonly used for coding and text editing?",
+    answers: [
+      "Photoshop",
+      "Vs Code",
+      "Premiere Pro",
+      "After Effects"
+    ],
+    correct: 1  // index of the correct answer in the answers array
+  },
+    {
+    text: "What does a “timeline” represent in editing software?",
+    answers: [
+      "File storage system",
+      "Sequence of edits over time",
+      "Internet speed",
+      "Keyboard shortcuts"
+    ],
+    correct: 1  // index of the correct answer in the answers array
+  },
+    {
+    text: "What is a keyframe used for in editing?",
+    answers: [
+      "Saving a file",
+      "Marking start/end of an animation change",
+      "Exporting video",
+      "Deleting layers"
+    ],
+    correct: 1  // index of the correct answer in the answers array
+  },
+  {
+    text: "What is the purpose of the Preview button in editing software?",
+    answers: [
+      "Delete the project",
+      "Test how the final edit looks or plays",
+      "Rename files",
+      "Create a new folder"
+    ],
+    correct: 1 // index of the correct answer in the answers array
+  },
+
+]
+
+
+let currentIndex = 0
+let score = 0
+
+function loadQuestion(index) {
+    //questions index stored inside question variable
+    const question = questions[index];
+
+    const questionafter = index + 1
+    //update question number
+    questionNumber.textContent = "Question " + questionafter + " of " + questions.length;
+
+    //update text
+    questionText.textContent = question.text;
+
+// search obj in array
+    for (let i = 0; i < btnsArray.length; i++) {
+        //put question answers into array
+        btnsArray[i].textContent = question.answers[i];
+    }
+
+    nextBtn.classList.add('hidden')
+
+    questionCard.classList.remove('answered')
+    
+
+
+    
+
+}
+
+
+
+
+
+
 // based on id, store in variable(section)
 const gameTitle = document.getElementById("game-title")
 const scoreDisplay = document.getElementById("score")
@@ -15,11 +105,17 @@ const answerBtnsNodeList = document.querySelectorAll(".answer-btn")
 console.log(answerBtnsCollection);
 console.log(answerBtnsNodeList);
 
+//store inside array
 const btnsArray = Array.from(answerBtnsNodeList)
 
+
+//change content
 gameTitle.textContent = "⚡ Quick Fire Trivia"
 
+
+
 console.log("First question:", questionText.textContent)
+//make it uppercase
 questionNumber.textContent = questionNumber.textContent.toUpperCase()
 
 
@@ -36,3 +132,4 @@ console.log("The <ul> that holds all buttons:", firstLi.parentElement)
 questionCard.classList.remove("answered")
 // Back to normal
 
+loadQuestion(0)
